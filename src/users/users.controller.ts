d5ca431f2af4011aaa9a40a6da684be16fca7b9f
Navish Bansal <navishbansal@MacBookPro.lan>
2025-04-06 08:17:31 -0600

    api changes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post, Body, Get, Req } from '@nestjs/common';
+import { Controller, Post, Body, Get, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { get } from 'http';
 import { User } from './entities/user.entity';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('user')
 export class UsersController {
@@ -13,6 +14,7 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('settings')
   getSettings(@Req() request: Request & { user: Partial<User> }) {
     const user = request.user;
